0b3a8c0db78eadcf349e37abd47e14bdb18e73eb
Brendan <brendancarter43@gmail.com>
2019-07-06 17:52:08 -0500

    Add project files.

---
+++ ordaba/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Brendan Carter\Source\Repos\ordaba\ordaba\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
